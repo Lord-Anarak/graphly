cf1f7d852533348ee080616382fad501973fe7d0
Lord Anarak <afrarpro95@gmail.com>
2025-01-06 07:00:58 +0400

    works page

--- src/pages/works/index.jsx
+++ src/pages/works/index.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import TextAnimation from "@/components/TextAnimation";
